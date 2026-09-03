8f1605245c884e4ad6faaa1572bbccfeaaf4829d
Ofentse Magidela <magidelaofentse2@gmail.com>
2026-09-03 23:07:55 +0200

    chore(logging): add logging to WhatsApp consumer

--- src/main/java/com/ofentse/pulse/notification/whatsapp/consumer/WhatsAppNotificationConsumer.java
+++ src/main/java/com/ofentse/pulse/notification/whatsapp/consumer/WhatsAppNotificationConsumer.java
@@ -3,12 +3,15 @@ package com.ofentse.pulse.notification.whatsapp.consumer;
 import com.ofentse.pulse.notification.config.RabbitMQConfig;
 import com.ofentse.pulse.notification.whatsapp.WhatsAppService;
 import com.ofentse.pulse.notification.whatsapp.dto.WhatsAppNotificationMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WhatsAppNotificationConsumer {
 
+    private static final Logger log = LoggerFactory.getLogger(WhatsAppNotificationConsumer.class);
     private final WhatsAppService whatsAppService;
 
     public WhatsAppNotificationConsumer(WhatsAppService whatsAppService) {
@@ -17,6 +20,9 @@ public class WhatsAppNotificationConsumer {
 
     @RabbitListener(queues = RabbitMQConfig.WHATSAPP_QUEUE)
     public void consumeWhatsApp(WhatsAppNotificationMessage message) {
+
+        log.info("Received WhatsApp notification {}", message.getNotificationId());
+
         whatsAppService.sendWhatsApp(message);
     }
 }
